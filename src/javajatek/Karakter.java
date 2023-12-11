b49b8be6f4cc75f99713f876940142688609afbd
kereszteshunor <kereszteshunor36@gmail.com>
2023-12-11 08:48:54 +0100

    felszereles

--- src/javajatek/Karakter.java
+++ src/javajatek/Karakter.java
@@ -1,7 +1,11 @@
 package javajatek;
 
+import java.util.ArrayList;
+
 public class Karakter
 {
+    private ArrayList<String> felszereles;
+    
     private int eletero;
     private int ugyesseg;
     private int szerencse;
